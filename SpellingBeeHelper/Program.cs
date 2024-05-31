4e34c0cd25c644ff3abcdff6ac14dfeeb5ba65e9
Brendan MacLean <brendan.maclean@gmail.com>
2024-05-31 16:21:13 -0700

    Initial commit with working solution that regularly helps to achieve Queen Bee in under 10 minutes.

---
+++ SpellingBeeHelper/Program.cs
@@ -0,0 +1,147 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SpellingBeeHelper;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    // Possible dictionaries to search
+    private static string dictionaryDir = @"C:\Users\brend\source\repos\SpellingBeeHelper\SpellingBeeHelper";
+    private static string dictionaryPath = Path.Combine(dictionaryDir, "usa2.txt");    // small
+    private static string seenPath = Path.Combine(dictionaryDir, "seen.txt");    // seen but not in dictionaries
+    private static string notWordsPath = Path.Combine(dictionaryDir, "notwords.txt");    // seen but not in dictionaries
+    private static string dictionaryLargePath = Path.Combine(dictionaryDir, "en_US-large.txt"); // medium
+    private static string dictionaryHugePath = Path.Combine(dictionaryDir, "words_alpha.txt"); // large
+    private static string dictionaryScrabblePath = Path.Combine(dictionaryDir, "scrabble-dictionary.txt"); // large
+    private static string dictionaryWebstersPath = Path.Combine(dictionaryDir, "WebstersEnglishDictionary.txt");
+
+    private static void Main(string[] args)
+    {
+        // Ask for the set of letters allowed
+        string? beeChars = null;
+        while (beeChars == null)
+        {
+            Console.WriteLine("Letters (7 char with center first): ");
+            string? inputChars = Console.ReadLine();
+            if (inputChars == null)
+                continue;
+            inputChars = inputChars.ToLowerInvariant();
+            if (inputChars.Distinct().Count() == 7 && inputChars.All(c => 'a' <= c && c <= 'z'))
+                beeChars = inputChars;
+        }
+
+        // Find base set of words usually achieving "genius" level
+        var trie = new Trie(dictionaryPath, seenPath);
+        var anagrams = trie.FindBeeWords(beeChars);
+        Console.WriteLine();
+        var notWords = File.ReadLines(notWordsPath);
+        ShowChoices(notWordsPath, anagrams.Except(notWords).ToArray());
+
+        // Find larger set with more misses, adding found words to seen.txt
+        var trieLarge = new Trie(dictionaryLargePath);
+        var anagramsLarge = trieLarge.FindBeeWords(beeChars);
+        var anagramsDiff = anagramsLarge.Except(anagrams).Except(notWords).ToArray();
+        ShowChoices(seenPath, anagramsDiff, notWordsPath);
+
+        // Search HUGE set of words by start character and length
+        // adding found words to seen.txt
+        var trieHuge = new Trie(dictionaryHugePath,
+            dictionaryLargePath, dictionaryPath, dictionaryScrabblePath, dictionaryWebstersPath);
+        char? searchLetter;
+        int? searchLength;
+        for (; ;)
+        {
+            Console.WriteLine();
+            searchLetter = GetSearchLetter();
+            searchLength = GetSearchLength();
+            if (!searchLetter.HasValue && !searchLength.HasValue)
+                break;
+
+            var anagramsHuge = trieHuge.FindBeeWords(beeChars, searchLetter, searchLength)
+                .Except(notWords)
+                // .Except(new[] { anagrams, anagramsLarge}.SelectMany(a => a)) - missing words
+                .ToArray();
+            ShowChoices(seenPath, anagramsHuge);
+        }
+
+        static char? GetSearchLetter()
+        {
+            for (; ; )
+            {
+                Console.Write("Letter: ");
+                string? letterText = Console.ReadLine();
+                if (string.IsNullOrEmpty(letterText))
+                    return null;
+                if (letterText.Length == 1)
+                {
+                    var ch = char.ToLowerInvariant(letterText[0]);
+                    if (char.IsAsciiLetterLower(ch))
+                        return ch;
+                }
+                Console.WriteLine("Error: please enter a letter between a and z.");
+            }
+        }
+
+        static int? GetSearchLength()
+        {
+            for (; ; )
+            {
+                Console.Write("Length: ");
+                string? lengthText = Console.ReadLine();
+                if (string.IsNullOrEmpty(lengthText))
+                    return null;
+                int searchLength;
+                if (int.TryParse(lengthText, out searchLength))
+                    return searchLength;
+            }
+        }
+    }
+
+    private static void ShowChoices(string seenPath, string[] anagrams, string notPath = null)
+    {
+        int line = 1;
+        foreach (var anagram in anagrams)
+        {
+            Console.Write(line++ + ". ");
+            Console.WriteLine(anagram);
+        }
+        for (; ;)
+        {
+            Console.Write("Seen (comma separated)? ");
+            var seenWords = Console.ReadLine();
+            if (string.IsNullOrEmpty(seenWords))
+            {
+                WriteNotSeenWords(notPath, anagrams, []);
+                return;
+            }
+
+            using var seenWriter = new StreamWriter(seenPath, true);
+            try
+            {
+                var seenWordIndexes = seenWords.Split(',').Select(w => int.Parse(w.Trim()) - 1).ToArray();
+                foreach (var i in seenWordIndexes)
+                {
+                    seenWriter.WriteLine(anagrams[i]);
+                }
+                WriteNotSeenWords(notPath, anagrams, seenWordIndexes);
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+
+    private static void WriteNotSeenWords(string notPath, string[] anagrams, int[] seenWordIndexes)
+    {
+        if (notPath != null)
+        {
+            using var notWriter = new StreamWriter(notPath, true);
+            for (int i = 0; i < anagrams.Length; i++)
+            {
+                if (!seenWordIndexes.Contains(i))
+                    notWriter.WriteLine(anagrams[i]);
+            }
+        }
+    }
+}
---
+++ SpellingBeeHelper/Trie.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellingBeeHelper
+{
+    internal class Trie
+    {
+        private bool _isWord;
+        private Trie[] _edges;
+
+        public Trie()
+        {
+            _edges = new Trie[26];
+        }
+
+        public Trie(IList<string> words)
+            : this()
+        {
+            AddWords(words);
+        }
+
+        public Trie(params string[] wordListPaths)
+            : this()
+        {
+            AddWords(wordListPaths);
+        }
+
+        private int ToIndex(char c)
+        {
+            return char.ToLowerInvariant(c) - 'a';
+        }
+
+        public void AddWords(params string[] wordListPaths)
+        {
+            foreach (var wordListPath in wordListPaths)
+            {
+                var wordList = File.ReadAllLines(wordListPath)
+                    .Where(l => l.Length > 0 && l.All(char.IsAsciiLetter))
+                    .Select(l => l.ToLower()).Distinct().ToList();
+                AddWords(wordList);
+            }
+        }
+        public void AddWords(IList<string> words)
+        {
+            foreach (string word in words)
+            {
+                if (word.Length > 0)
+                {
+                    AddWord(word);
+                }
+            }
+        }
+
+        private void AddWord(string word)
+        {
+            if (word.Length == 0)
+            {
+                _isWord = true;
+                return;
+            }
+            int edge = ToIndex(word[0]);
+            var nextNode = _edges[edge];
+            if (nextNode == null)
+            {
+                nextNode = _edges[edge] = new Trie();
+            }
+            string remainder = word.Substring(1);
+            nextNode.AddWord(remainder);
+        }
+
+        private bool IsWord(string word)
+        {
+            if (word.Length == 0)
+            {
+                return _isWord;
+            }
+            int edge = ToIndex(word[0]);
+            var nextNode = _edges[edge];
+            if (nextNode == null)
+                return false;
+            string remainder = word.Substring(1);
+            return nextNode.IsWord(remainder);
+        }
+
+        public IList<string> FindWords(string chars, string prefix = "")
+        {
+            var result = new List<string>();
+            if (_isWord)
+            {
+                result.Add(prefix);
+            }
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int edge = ToIndex(chars[i]);
+                var nextNode = _edges[edge];
+                if (nextNode != null)
+                    result.AddRange(nextNode.FindWords(chars, prefix + chars[i]));
+            }
+            return result;
+        }
+
+        public IList<string> FindBeeWords(string beeChars, char? letter = null, int? length = null)
+        {
+            var anagrams = FindWords(beeChars)
+                .Where(w => w.Length > 3 && w.Contains(beeChars[0]))
+                .Order().ToList();
+            if (letter.HasValue)
+                anagrams = anagrams.Where(w => w[0] == letter.Value).ToList();
+            if (length.HasValue)
+                anagrams = anagrams.Where(w => w.Length == length.Value).ToList();
+            return anagrams;
+        }
+    }
+}
